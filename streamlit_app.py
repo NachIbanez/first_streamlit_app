ea0d949453a76247af638423e6af16b34b6432e2
NachIbanez <43443842+NachIbanez@users.noreply.github.com>
2022-05-05 12:53:37 +0200

    Update streamlit_app.py
    added panda's library
    added some variable and functions for doing a list of fruits using panda and streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,27 @@
-#Import libraries
+####################
+##Import libraries##
+####################
+
 import streamlit
 import pandas
+
+#####################################
+#Variable declarations and functions#
+#####################################
 
 #Panda's function to read CSV FILES 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-# Start of the script
+#Let's put a pick up list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+
+#Display the table of the page
+streamlit.dataframe(my_fruit_list)
+
+
+#####################
+#Start of the script#
+#####################
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
