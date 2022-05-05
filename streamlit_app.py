b9df671fe66941be9a4e128ec7aba61a347148f4
NachIbanez <43443842+NachIbanez@users.noreply.github.com>
2022-05-05 12:56:40 +0200

    Update streamlit_app.py
    added fruit names in indexes instead of numbers

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ import pandas
 
 #Panda's function to read CSV FILES 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 #####################
