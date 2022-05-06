5bd76f099c7094eb11911c042fc9acb0cebc6128
NachIbanez <43443842+NachIbanez@users.noreply.github.com>
2022-05-06 10:34:04 +0200

    Update streamlit_app.py
    add requests library

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@
 
 import streamlit
 import pandas
+import requests
 
 #####################################
 #Variable declarations and functions#
@@ -12,6 +13,9 @@ import pandas
 #Panda's function to read CSV FILES 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 #####################
