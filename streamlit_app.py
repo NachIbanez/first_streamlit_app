5b0ca459923d68d1cccdd23a17d5fc56da4febdc
NachIbanez <43443842+NachIbanez@users.noreply.github.com>
2022-05-06 11:15:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,6 @@ import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
 
 #####################
 #Start of the script#
@@ -45,6 +43,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlite.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalize it
 
